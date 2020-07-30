8411d7e2743f4f9f2255684a87b6884481c046e6
Mark Butcher <markjamesbutcher@gmail.com>
2020-07-30 21:24:47 +1000

    redux state handling for login form page

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -1,20 +1,42 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 import { Text, View } from 'react-native';
 import CardSection from './common/CardSection';
 import Card from './common/Card';
 import Input from './common/Input';
 import Button from './common/Button';
 
+import { emailChange, passwordChange } from '../actions';
+
 class LoginForm extends Component {
+  onEmailChange(text) {
+    this.props.emailChange(text);
+  }
+
+  onPasswordChange(text) {
+    this.props.passwordChange(text);
+  }
+
   render() {
     return (
       <View style={{ paddingTop: 50 }}>
         <Card>
           <CardSection>
-            <Input label="Email" placeholder="email@email.com" />
+            <Input
+              label="Email"
+              placeholder="email@email.com"
+              onChangeText={this.onEmailChange.bind(this)}
+              value={this.props.email}
+            />
           </CardSection>
           <CardSection>
-            <Input secureText label="Password" placeholder="password" />
+            <Input
+              secureText
+              label="Password"
+              placeholder="password"
+              onChangeText={this.onPasswordChange.bind(this)}
+              value={this.props.password}
+            />
           </CardSection>
           <CardSection>
             <Button>
@@ -27,4 +49,11 @@ class LoginForm extends Component {
   }
 }
 
-export default LoginForm;
+const mapStateToProps = (state) => {
+  return {
+    email: state.auth.email,
+    password: state.auth.password,
+  };
+};
+
+export default connect(mapStateToProps, { emailChange, passwordChange })(LoginForm);
