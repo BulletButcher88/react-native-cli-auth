262fb4331f81b1fea7c43ee945206d9061bbcb3e
Mark Butcher <markjamesbutcher@gmail.com>
2020-07-31 13:18:32 +1000

    login erro and user create login

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,5 +1,5 @@
 import firebase from 'firebase';
-import { EMAIL_CHANGED, PASSWORD_CHANGED, LOGIN_USER_SUCCESS } from './types';
+import { EMAIL_CHANGED, PASSWORD_CHANGED, LOGIN_USER_SUCCESS, LOGIN_ERROR } from './types';
 
 export const emailChange = (text) => {
   return {
@@ -18,8 +18,25 @@ export const passwordChange = (text) => {
 export const loginUser = ({ email, password }) => {
   return (dispatch) => {
     firebase.auth().signInWithEmailAndPassword(email, password)
-      .then((user) => {
-        dispatch({ type: LOGIN_USER_SUCCESS, payload: user });
+      .then((user) => loginSuccess(dispatch, user))
+      .catch(() => {
+        firebase.auth().createUserWithEmailAndPassword(email, password)
+          .then((user) => loginSuccess(dispatch, user))
+          .catch((err) => loginUserFail(dispatch, err));
       });
   };
+};
+
+const loginUserFail = (dispatch, err) => {
+  dispatch({
+    type: LOGIN_ERROR,
+    payload: err.message,
+  });
+};
+
+const loginSuccess = (dispatch, user) => {
+  dispatch({
+    type: LOGIN_USER_SUCCESS,
+    payload: user,
+  });
 };
