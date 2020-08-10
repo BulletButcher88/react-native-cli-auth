4c3cc405b57d43477a4777cd5d0a0dbd5746e347
Mark Butcher <markjamesbutcher@gmail.com>
2020-08-11 00:34:45 +1000

    redux clear content of to state

--- src/actions/EmployeeAction.js
+++ src/actions/EmployeeAction.js
@@ -1,5 +1,6 @@
 import firebase from 'firebase';
-import { EMPLOYEE_UPDATE } from './types';
+import { EMPLOYEE_UPDATE, EMPLOYEE_CREATE } from './types';
+import { Actions } from 'react-native-router-flux';
 
 export const employeeUpdate = ({ prop, value }) => {
   return {
@@ -10,6 +11,13 @@ export const employeeUpdate = ({ prop, value }) => {
 
 export const employeeCreate = ({ name, phone, shift }) => {
   const { currentUser } = firebase.auth();
-  firebase.database().ref(`/users/${currentUser.uid}/employees`)
-    .push({ name, phone, shift });
+  return (dispatch) => {
+    firebase.database().ref(`/users/${currentUser.uid}/employees`)
+      .push({ name, phone, shift })
+      .then(() => {
+        dispatch({ type: EMPLOYEE_CREATE });
+        Actions.pop({ type: 'reset' })
+      });
+    //the type: 'reset' above takes out the back-button that normally shows at the top of the page in the admin page
+  };
 };
