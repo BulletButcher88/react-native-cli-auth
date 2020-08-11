bf0aa8981ee03176ba54ae679178cf2e981bf6e6
Mark Butcher <markjamesbutcher@gmail.com>
2020-08-11 22:57:22 +1000

    reconfigured from ListView to FlatList react-native component

--- src/components/EmployeeList.js
+++ src/components/EmployeeList.js
@@ -1,26 +1,57 @@
+import _ from 'lodash';
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import { View, Text } from 'react-native';
-import { employeeFetch } from '../actions/EmployeeAction'
+import { View, Text, FlatList, StyleSheet } from 'react-native';
+import { employeeFetch } from '../actions/EmployeeAction';
 
 class EmployeeList extends Component {
-
-  componentWillMount() {
+  UNSAFE_componentWillMount() {
     this.props.employeeFetch();
   }
 
-  render() {
+  createDataSource(employees) {
+    let employeeArr = [];
+    if (employees) {
+      employeeArr = Object.keys(employees).map(function (k, y) {
+        let empOjb = employees[k];
+        empOjb.key = k;
+        return empOjb;
+      });
+    }
     return (
-      <View>
-        <Text>LIST OF WORKERS</Text>
-        <Text>LIST OF WORKERS</Text>
-        <Text>LIST OF WORKERS</Text>
-        <Text>LIST OF WORKERS</Text>
-        <Text>LIST OF WORKERS</Text>
-      </View>
+      <FlatList
+        data={employeeArr}
+        renderItem={({ item }) => <Text style={styles.item}>{item.name}</Text>}
+      />
     );
   }
+
+  render() {
+    if (this.props.employees) {
+      return (
+        <View>
+          {this.createDataSource(this.props.employees)}
+        </View>
+      );
+    }
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    paddingTop: 22,
+  },
+  item: {
+    padding: 10,
+    fontSize: 18,
+    height: 44,
+  },
+})
+
+const mapStatesToProps = state => {
+  const employees = state.employees;
+  return { employees };
 };
 
-
-export default connect(null, { employeeFetch })(EmployeeList);
+export default connect(mapStatesToProps, { employeeFetch })(EmployeeList);
